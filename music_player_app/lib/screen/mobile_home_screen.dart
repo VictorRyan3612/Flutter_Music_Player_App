9e784fa168482d2807d955c3f56771f706cf9a75
Victor Ryan <vitorsilva3612@gmail.com>
2023-12-24 17:07:15 -0300

    Listagem padrao

--- music_player_app/lib/screen/mobile_home_screen.dart
+++ music_player_app/lib/screen/mobile_home_screen.dart
@@ -7,8 +7,32 @@ class MobileHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Mobile screen")
+      appBar: AppBar(
+        title: Text("Music"),
+        leading: Icon(Icons.menu),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("Music Name", style: TextStyle(fontSize: 20),),
+              subtitle: Text("Artist Name - Album Name", style: TextStyle(fontSize: 15)),
+              leading: Icon(Icons.music_note, size: 32),
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "3:30",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       )
     );
   }
