41d6bdb54597d9ad181b4e7ef74f01ada0558ead
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-10 19:36:12 -0300

    MusicDataService inicial

---
+++ music_player_app/lib/data/music_data_service.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MusicDataService{
+  ValueNotifier<List<String>> listFoldersPaths = ValueNotifier([]);
+  ValueNotifier<List<String>> listPaths = ValueNotifier([]);
+
+  loadfolderPath(){
+    
+  }
+}
+
+MusicDataService musicDataService = MusicDataService();
--- music_player_app/lib/main.dart
+++ music_player_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:music_player_app/data/music_data_service.dart';
 import 'package:music_player_app/layout/layout.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +11,17 @@ void main() {
 class MainApp extends HookWidget {
   const MainApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+
+    musicDataService.loadfolderPath();
+
+    
     return MaterialApp(
       // theme: finalTheme,
       debugShowCheckedModeBanner: false,
+      
 
 
       initialRoute: '/',
