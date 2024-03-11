45f6c75bd3bfc1214614cef645f9288a53581938
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-11 20:29:59 -0300

    Mudanca de tema
    Pego e adaptado de https://github.com/VictorRyan3612/Flutter_Notes_App/blob/main/app_notes/lib/screen/config_screen.dart

--- music_player_app/lib/main.dart
+++ music_player_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:music_player_app/config/settings_data_service.dart';
+import 'package:music_player_app/config/theme_config.dart';
 import 'package:music_player_app/data/music_data_service.dart';
 import 'package:music_player_app/layout/layout.dart';
 import 'package:music_player_app/screen/config_screen.dart';
 import 'package:music_player_app/view/builder_layout.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main() {
@@ -17,19 +19,38 @@ class MainApp extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    settingsService.loadSettings();
+        // base states 
+    final currentIsDarkMode = useState(true); //Theme
+    final currentColor = useState('Blue'); // Accent color
+
+    Future<void> loadSettings() async {
+      final prefs = await SharedPreferences.getInstance();
+      
+      final isDarkMode = prefs.getBool('isDarkMode') ?? true;
+      currentIsDarkMode.value = isDarkMode;
+
+      final colorTheme = prefs.getString('colorTheme') ?? 'Blue';
+      currentColor.value = colorTheme;
+
+    }
+
+    loadSettings();
+    final finalTheme = setTheme(currentIsDarkMode.value, currentColor.value);
 
     return MaterialApp(
-      // theme: finalTheme,
+      theme: finalTheme,
       debugShowCheckedModeBanner: false,
       
 
       // home: BuilderLayout()
 
-      initialRoute: '/',
+      initialRoute: '/configs',
       routes: {
         '/': (context) => BuilderLayout(),
-        '/configs': (context) => ConfigScreen()
+        '/configs': (context) => ConfigScreen(
+          currentIsDarkMode: currentIsDarkMode,
+          currentColor: currentColor,
+          )
       }
     );
   }
