eb986523be2dc65e14bccd1334f15c59838c76bd
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-21 10:41:43 -0300

    playing_screen parte 1

--- music_player_app/lib/screen/music_playing_screen.dart
+++ music_player_app/lib/screen/music_playing_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:music_player_app/data/music_data_service.dart';
 
 class MusicPlayingScreen extends StatelessWidget {
   late Offset _initialPosition;
@@ -10,9 +13,8 @@ class MusicPlayingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Scaffold(
-        body: Center(child: Text('Texto'),),
-      ),
+      child: ScreenMusic(),
+
       
       onVerticalDragStart: (details) {
         _initialPosition = details.globalPosition;
@@ -24,6 +26,56 @@ class MusicPlayingScreen extends StatelessWidget {
           Navigator.pop(context);
         }
       },
+    );
+  }
+}
+class ScreenMusic extends StatelessWidget {
+  const ScreenMusic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shuffle),
+            onPressed: () {
+
+            }),IconButton(
+            icon: Icon(Icons.repeat),
+            onPressed: () {
+
+            }),IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+
+            }),
+        ],
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${musicDataService.actualPlayingMusic.value.trackName}', style: TextStyle(fontSize: 22),),
+                  Text('${musicDataService.actualPlayingMusic.value.albumArtistName} - ${musicDataService.actualPlayingMusic.value.albumName}'),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.memory(musicDataService.actualPlayingMusic.value.albumArt!, width: 400, height: 400,),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
