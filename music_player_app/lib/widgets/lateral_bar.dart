4b84b63ce5cc6e5d41b3abdb40a469261d4e35d2
Victor Ryan <vitorsilva3612@gmail.com>
2024-03-29 13:20:39 -0300

    refact: lateral bar parte 1 #1

--- music_player_app/lib/widgets/lateral_bar.dart
+++ music_player_app/lib/widgets/lateral_bar.dart
@@ -8,98 +8,95 @@ class ExtensibleLateralBar extends HookWidget {
   Widget build(BuildContext context) {
     Color colorContainer = Theme.of(context).brightness == Brightness.dark ? Colors.black : Colors.white;
     var isOpen = useState(false);
-    return Builder(
-      builder: (context) {
-        if (isOpen.value) {
-          return Expanded(
-            flex: 2,
-            child: Container(
-              color: colorContainer,
-              child: ListView(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: () {
-                      isOpen.value = !isOpen.value;
-                    }, 
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.home_filled),
-                    title: Text("Inicio"),
-                    onTap: () {
-                      
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.people),
-                    title: Text("Artistas"),
-                    onTap: () {
-                      
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.library_music),
-                    title: Text("Albums"),
-                    onTap: () {
-                      
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text("Configuracoes"),
-                    onTap: () {
-                      Navigator.pushNamed(context, '/configs');
-                    },)
-                ],
-              ),
-            ),
-          );
-        } else {
-          return Container(
-            color: colorContainer,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    onPressed: () {
-                      isOpen.value = !isOpen.value;
-                    }, 
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.home_filled),
-                    onPressed: () {
-                      
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.people),
-                    onPressed: () {
-                      
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.library_music),
-                    onPressed: () {
-                      
-                    },
-                  ),
-                  Divider(),
-
-                  IconButton(
-                    icon: Icon(Icons.settings),
-                    onPressed: (){
-                      Navigator.pushNamed(context, '/configs');
+    return Expanded(
+      flex: isOpen.value == true ? 2 : 0,
+      child: Container(
+        color: colorContainer,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Builder(
+            builder: (context) {
+              if(isOpen.value == true){
+                return Column(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        isOpen.value = !isOpen.value;
+                      }, 
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.home_filled),
+                      title: Text("Inicio"),
+                      onTap: () {
+                        
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.people),
+                      title: Text("Artistas"),
+                      onTap: () {
+                        
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.library_music),
+                      title: Text("Albums"),
+                      onTap: () {
+                        
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: Icon(Icons.settings),
+                      title: Text("Configuracoes"),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/configs');
                       },
                     )
-                ]
-              ),
-            ),
-          );
-        }
-      },
+                  ]
+                );
+              }
+              else{
+                return Column(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {
+                        isOpen.value = !isOpen.value;
+                      }, 
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.home_filled),
+                      onPressed: () {
+                        
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.people),
+                      onPressed: () {
+                        
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.library_music),
+                      onPressed: () {
+                        
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.settings),
+                      onPressed: (){
+                        Navigator.pushNamed(context, '/configs');
+                      },
+                    )
+                  ]
+                );
+              }
+            }
+          ),
+        ),
+      ),
     );
   }
 }
