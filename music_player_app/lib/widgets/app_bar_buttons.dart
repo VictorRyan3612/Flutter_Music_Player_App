3dc4d391b692dc3b3cbd6e9365974ea69347ee71
Victor Ryan <vitorsilva3612@gmail.com>
2024-05-20 09:31:49 -0300

    feat: numbers musics and selecting

--- music_player_app/lib/widgets/app_bar_buttons.dart
+++ music_player_app/lib/widgets/app_bar_buttons.dart
@@ -11,12 +11,34 @@ class AppBarButtons extends StatelessWidget implements PreferredSizeWidget{
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.black26,
-      title: ValueListenableBuilder(
-        valueListenable: musicDataService.musicsValueNotifier,
-        builder: (context, value, child) {
-          return Text('${value['data'].length}');
-        }
-      ),
+      title: NumberMusics(),
+    );
+  }
+}
+
+class NumberMusics extends StatelessWidget {
+  const NumberMusics({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: musicDataService.musicsValueNotifier,
+      builder: (context, musicsValueNotifier, child) {
+        return ValueListenableBuilder(
+          valueListenable: musicDataService.newplaylist,
+          builder: (context, newplaylist, child) {
+            if (newplaylist.isEmpty){
+              return Text('${musicsValueNotifier['data'].length}');
+            }
+            else{
+              return Text('${newplaylist.length}/${musicsValueNotifier['data'].length}');
+
+            }
+          },
+        );
+      }
     );
   }
 }
