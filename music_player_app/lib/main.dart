ee27534d388ce52b1fb8f3304ca70111011ff31a
Victor Ryan <vitorsilva3612@gmail.com>
2024-07-02 21:22:10 -0300

    feat: vicrMaterialApp

--- music_player_app/lib/main.dart
+++ music_player_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_vicr_widgets/flutter_vicr_widgets.dart';
 
 import 'package:music_player_app/config/settings_data_service.dart';
 import 'package:music_player_app/config/theme_config.dart';
 import 'package:music_player_app/data/music_data_service.dart';
-import 'package:music_player_app/layout/layout.dart';
-import 'package:music_player_app/screen/config_screen.dart';
+// import 'package:music_player_app/screen/config_screen.dart';
 import 'package:music_player_app/screen/desktop_home_screen.dart';
 import 'package:music_player_app/screen/mobile_home_screen.dart';
 
@@ -20,45 +20,16 @@ void main() {
         shuffle: settingsService.shuffle
       );
     });
-  runApp(const MainApp());
-}
-
-class MainApp extends HookWidget {
-  const MainApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    // base states 
-    final currentIsDarkMode = useState(settingsService.isDarkMode.value); //Theme
-    final currentColor = useState(settingsService.colorName.value); // Accent color
-
     
-      
+  VictMaterialApp().loadSettings();
 
-    final finalTheme = setTheme(currentIsDarkMode.value, currentColor.value);
-
-    return MaterialApp(
-      theme: finalTheme,
-      debugShowCheckedModeBanner: false,
-      
-
-      // home: BuilderLayout()
-
-      initialRoute: '/',
-      routes: {
-        // '/': (context) => LayoutDecididor2(),
-        
-        '/': (context) => LayoutDecider(
-          isMobile: settingsService.isMobile,
-          optionMobile: MobileHomeScreen(),
-          optionDesktop: DesktopHomeScreen(),
-        ),
-        '/configs': (context) => ConfigScreen(
-          currentIsDarkMode: currentIsDarkMode,
-          currentColor: currentColor,
-          )
-      }
-    );
-  }
+  runApp(VictMaterialApp(
+    routes: {
+      '/': (context) => LayoutDecider(
+        isMobile: settingsService.isMobile,
+        optionMobile: MobileHomeScreen(),
+        optionDesktop: DesktopHomeScreen(),
+      ),
+    }
+  ));
 }
