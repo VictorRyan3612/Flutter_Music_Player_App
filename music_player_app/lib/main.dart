5430241c41398e97d8352990fca5e5d6d30f99f9
Victor Ryan <vitorsilva3612@gmail.com>
2024-07-03 20:54:18 -0300

    feat: remocao configScreen e uso de configWidgets

--- music_player_app/lib/main.dart
+++ music_player_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_vicr_widgets/flutter_vicr_widgets.dart';
@@ -5,7 +6,6 @@ import 'package:flutter_vicr_widgets/flutter_vicr_widgets.dart';
 import 'package:music_player_app/config/settings_data_service.dart';
 import 'package:music_player_app/config/theme_config.dart';
 import 'package:music_player_app/data/music_data_service.dart';
-// import 'package:music_player_app/screen/config_screen.dart';
 import 'package:music_player_app/screen/desktop_home_screen.dart';
 import 'package:music_player_app/screen/mobile_home_screen.dart';
 
@@ -24,6 +24,9 @@ void main() {
   VictMaterialApp().loadSettings();
 
   runApp(VictMaterialApp(
+    configWidget: ConfigWidgets(
+      trailingWidgets: listWidgets
+    ),
     routes: {
       '/': (context) => LayoutDecider(
         isMobile: settingsService.isMobile,
@@ -33,3 +36,76 @@ void main() {
     }
   ));
 }
+
+
+List<Widget> listWidgets = [
+  ListTile(
+    title: Text("Modo de Visualização em Tabela"),
+    trailing: ValueListenableBuilder(
+      valueListenable: settingsService.isTable,
+      builder: (context, valueIsTable, child) {
+        return Switch(
+          value: valueIsTable, 
+          onChanged: (valueOnChanged) {
+            settingsService.isTable.value = valueOnChanged;
+            settingsService.saveSettings();
+          },
+        );
+      }
+    ),
+    
+  ),
+
+  ListTile(
+    title: Text("Repetição da musica na fila de reprodução se em loop"),
+    subtitle: Text("Com loop ativado, a música deve aparecer mais uma vez na fila de reprodução"),
+    trailing: ValueListenableBuilder(
+      valueListenable: settingsService.addRepeat,
+      builder: (context, valueAddRepeat, child) {
+        return Switch(
+          value: valueAddRepeat, 
+          onChanged: (valueOnChanged) {
+            settingsService.addRepeat.value = valueOnChanged;
+            musicDataService.addRepeat = valueOnChanged;
+            print('settings: ${settingsService.addRepeat.value}\ndata:${musicDataService.addRepeat}');
+            settingsService.saveSettings();
+          },
+        );
+      }
+    ),
+    
+  ),
+  
+  ListTile(
+    title: Text("Selecionar Pastas"),
+    onTap: () async {
+      String? folder = await FilePicker.platform.getDirectoryPath();
+      
+      if (folder != null) {
+        print('Pasta selecionada: $folder');
+        settingsService.saveSettings();
+        musicDataService.addFolderPath(folder);
+      } else {
+        print('Nenhuma pasta selecionada.');
+      }
+
+    },
+  ),
+
+  ListTile(
+    title: Text("Remover Pastas"),
+    onTap: () async {
+      String? folder = await FilePicker.platform.getDirectoryPath();
+      
+      if (folder != null) {
+        print('Pasta selecionada: $folder');
+        settingsService.saveSettings();
+        musicDataService.removeFolderPath(folder);
+        
+      } else {
+        print('Nenhuma pasta selecionada.');
+      }
+
+    },
+  ),
+];
