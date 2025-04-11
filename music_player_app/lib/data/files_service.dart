fabc4094a72e57c3cf906b6028e77b2a390b5957
Victor Ryan <vitorsilva3612@gmail.com>
2025-04-11 16:02:40 -0300

    feat: FilesService saveJson

---
+++ music_player_app/lib/data/files_service.dart
@@ -0,0 +1,25 @@
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:path_provider/path_provider.dart';
+
+class FilesService {
+
+  void saveJson(Map<String, dynamic> musics) async{
+    Directory directory = await getApplicationSupportDirectory();
+    print(directory);
+    File file = File('${directory.path}\\allmusics.json');
+    print(file.path);
+    if(!file.existsSync()){
+      file.createSync();
+
+    }
+    String jsonString = jsonEncode(musics);
+    // file.writeAsStringSync(musicsValueNotifier.value['data'].toString());
+    var teste = Uint8List.fromList(utf8.encode(jsonString));
+    // file.writeAsSync(json.encode(musicsValueNotifier.value['data']));
+    file.writeAsBytesSync(teste, mode: FileMode.append);
+  }
+}
